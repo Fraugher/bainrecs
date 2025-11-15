2768464b3c209a6aa292084c744b21763676ab02
Fraugher <gooleyvt@gmail.com>
2025-11-15 13:34:17 -0500

    config tweaks

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def create_app():
 # if __name__ == '__main__':
 #     app.run(debug=True, host='127.0.0.1', port=5000)
 app = create_app()
-# For WSGI (PythonAnywhere uses this)
-application = app
+# # For WSGI (PythonAnywhere uses this)
+# application = app
 
 if __name__ == '__main__':
     with app.app_context():
