a7172242137971344c0c9cee28a541c53d9db1f3
Fraugher <gooleyvt@gmail.com>
2025-11-20 18:06:55 -0500

    post build stored procedures

--- apify_api/apify_endpoints.py
+++ apify_api/apify_endpoints.py
@@ -124,6 +124,8 @@ def pop_db():
             )
             db.session.add(new_review)
         try:
+            db.session.execute(db.text(current_app.config['DB_PROCEDURE_MAKE_RATINGS']))
+            db.session.execute(db.text(current_app.config['DB_PROCEDURE_MAKE_RESTAURANTS']))
             db.session.commit()
             msg=f"Successfully added {review_count} reviews to database"
         except Exception as e:
