7579bb406d1ee244aa24395360135a1b2ccbaa63
Fraugher <gooleyvt@gmail.com>
2025-11-20 19:12:48 -0500

    refine data model

--- models.py
+++ models.py
@@ -30,7 +30,6 @@ class Restaurant(db.Model):
     __tablename__ = 'restaurants'
     __table_args__ = {'extend_existing': True}
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     google_maps_id = db.Column(db.String(128))
     place_name = db.Column(db.String(255), nullable=False)
     place_address = db.Column(db.String(255))
